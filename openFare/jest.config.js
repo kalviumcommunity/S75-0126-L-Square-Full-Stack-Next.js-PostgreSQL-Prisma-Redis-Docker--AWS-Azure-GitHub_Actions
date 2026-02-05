263ef4f1adc03dbbb0ed191a333e2571dff1e36f
Dhanya Lakshmi s s <dhanyalakshmi.s.s.06@gmail.com>
2026-02-05 21:13:17 +0530

    Completed Unit Testing Framework Setup

---
+++ openFare/jest.config.js
@@ -0,0 +1,29 @@
+const nextJest = require('next/jest');
+const createJestConfig = nextJest({ dir: './' });
+
+const customJestConfig = {
+  setupFilesAfterEnv: ['<rootDir>/__tests__/setup.ts'],
+  testEnvironment: 'jsdom',
+  collectCoverage: true,
+  collectCoverageFrom: [
+    'src/**/*.{js,jsx,ts,tsx}',
+    '!src/**/*.test.{js,jsx,ts,tsx}',
+    '!src/pages/_*.{js,jsx,ts,tsx}',
+    '!src/app/**/*', // Exclude Next.js app router files for now
+    '!src/middleware.ts'
+  ],
+  testMatch: [
+    '**/__tests__/**/*.test.[jt]s?(x)',
+    '**/?(*.)+(spec|test).[jt]s?(x)'
+  ],
+  coverageThreshold: {
+    global: {
+      branches: 80,
+      functions: 80,
+      lines: 80,
+      statements: 80,
+    },
+  },
+};
+
+module.exports = createJestConfig(customJestConfig);
